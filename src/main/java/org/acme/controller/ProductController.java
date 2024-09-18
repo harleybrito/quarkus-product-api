b9d112fbad5a2e1e291b51511abf70933658d75c
harley.brito <harleydebrito@gmail.com>
2024-09-18 15:29:14 -0300

    commit inicial

---
+++ src/main/java/org/acme/controller/ProductController.java
@@ -0,0 +1,66 @@
+package org.acme.controller;
+
+import jakarta.inject.Inject;
+import jakarta.transaction.Transactional;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.DELETE;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import org.acme.dto.ProductDTO;
+import org.acme.service.ProductService;
+
+import java.util.List;
+
+@Path("/api/products")
+public class ProductController {
+    @Inject
+    ProductService productService;
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<ProductDTO> findAllProducts() {
+        return this.productService.getAllProducts();
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Transactional
+    public Response saveProduct(ProductDTO product) {
+        try {
+            this.productService.createNewProduct(product);
+            return Response.ok().build();
+        } catch (Exception e) {
+            return Response.serverError().build();
+        }
+    }
+
+    @PUT
+    @Path("/{id}")
+    @Transactional
+    public Response changeProduct(@PathParam("id") Long id, ProductDTO product) {
+        try {
+            this.productService.changeProduct(id, product);
+            return Response.ok().build();
+        } catch (Exception e) {
+            return Response.serverError().build();
+        }
+    }
+
+    @DELETE
+    @Path("/{id}")
+    @Transactional
+    public Response changeProduct(@PathParam("id") Long id) {
+        try {
+            this.productService.deleteProduct(id);
+            return Response.ok().build();
+        } catch (Exception e) {
+            return Response.serverError().build();
+        }
+    }
+}
---
+++ src/main/java/org/acme/service/ProductService.java
@@ -0,0 +1,62 @@
+package org.acme.service;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import org.acme.dto.ProductDTO;
+import org.acme.entity.ProductEntity;
+import org.acme.repository.ProductRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@ApplicationScoped
+public class ProductService {
+    @Inject
+    ProductRepository productRepository;
+
+    public List<ProductDTO> getAllProducts (){
+        List<ProductDTO> products = new ArrayList<>();
+        this.productRepository.findAll().stream().forEach(item -> {
+            products.add(this.entityToDTO(item));
+        });
+        return products;
+    }
+
+    public void createNewProduct(ProductDTO productDTO) {
+        this.productRepository.persist(this.DTOToEntity(productDTO));
+    }
+
+    public void deleteProduct(Long id) {
+        this.productRepository.deleteById(id);
+    }
+
+    public void changeProduct(Long id, ProductDTO productDTO) {
+        ProductEntity productEntity = this.productRepository.findById(id);
+        productEntity.setName(productDTO.getName());
+        productEntity.setPrice(productDTO.getPrice());
+        productEntity.setDescription(productDTO.getDescription());
+        productEntity.setCategory(productDTO.getCategory());
+        productEntity.setModel(productDTO.getModel());
+        this.productRepository.persist(productEntity);
+    }
+
+    private ProductEntity DTOToEntity(ProductDTO productDTO) {
+        ProductEntity productEntity = new ProductEntity();
+        productEntity.setName(productDTO.getName());
+        productEntity.setPrice(productDTO.getPrice());
+        productEntity.setDescription(productDTO.getDescription());
+        productEntity.setCategory(productDTO.getCategory());
+        productEntity.setModel(productDTO.getModel());
+        return productEntity;
+    }
+
+    private ProductDTO entityToDTO(ProductEntity productEntity) {
+        ProductDTO productDTO = new ProductDTO();
+        productDTO.setCategory(productEntity.getCategory());
+        productDTO.setName(productEntity.getName());
+        productDTO.setPrice(productEntity.getPrice());
+        productDTO.setDescription(productEntity.getDescription());
+        productDTO.setModel(productEntity.getModel());
+        return productDTO;
+    }
+}
